50e0357a263914454e887d5d60ff3094dcfd781b
NICOLAS SCHNEIDER LAPOLLI GARCIA <nicolas_s_garcia@estudante.sesisenai.org.br>
2024-04-17 12:32:32 -0300

    aa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Flexbox from './Componentes/Flexbox'
+import './App.css'
+
+function App(){
+
+    return(
+        
+        <Flexbox />
+    )
+    
+    
+}  
+export default App
---
+++ src/Componentes/Flexbox.jsx
@@ -0,0 +1,32 @@
+
+export default Flexbox;
+
+function Flexbox(){
+
+    return(
+<body>
+
+
+       <div className="box">
+
+        <div className="one">
+            <p>Texto 1</p>
+        </div>
+
+        <div className="one2">
+            <p>Texto 2</p>
+        </div>
+
+        <div className="one3">
+            <p>Texto 3</p>
+        </div>
+
+        </div>
+
+        
+
+</body>
+
+    )
+
+}
